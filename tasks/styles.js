c5822666ba8bbce99c74d52b84a3d8e2b2485f6d
Alexander <alexander@tundra.com.au>
2015-12-04 18:27:12 +1100

    styles: mobile mixin, merge media queries, make all mobile rules important

--- tasks/styles.js
+++ tasks/styles.js
@@ -1,4 +1,16 @@
 var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var mqpacker = require('css-mqpacker');
+
+var responsive = function (css, result) {
+	css.walkRules(function(rule) {
+		if (rule.parent !== result.root) {
+			rule.walkDecls(function(decl) {
+				decl.important = true;
+			});
+		}
+	});
+};
 
 module.exports = function (gulp, env, errorHandler) {
 
@@ -6,10 +18,15 @@ module.exports = function (gulp, env, errorHandler) {
 
 		return gulp.src(env.paths.src.sass).on("error", errorHandler)
 			.pipe( sass({
+				outputStyle: 'nested',
 				errLogToConsole: true,
 				includePaths: [env.paths.src.node + 'Ink/scss/']
 			}) )
-			.pipe( gulp.dest(env.paths.src.styles) );
+			.pipe( postcss([
+				require('css-mqpacker'),
+				responsive
+			]))
+			.pipe( gulp.dest(env.dist) );
 
 	});
 
